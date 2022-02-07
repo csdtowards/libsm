2861d9c5868f4b85e26ee3fd6c60d66d52198ad0
Yuan Liu <liuyuanvic@gmail.com>
2022-02-07 06:38:29 +0000

    format

--- src/sm2/ecc.rs
+++ src/sm2/ecc.rs
@@ -568,51 +568,6 @@ impl EccCtx {
         ret
     }
 
-    pub fn get_point(&self, x: &BigUint, neg: u8,) -> Result<Point, Sm2Error> {
-        let ctx = &self.fctx;
-
-        let x = FieldElem::from_biguint(x);
-
-        let x_cubic = ctx.mul(&x, &ctx.mul(&x, &x));
-        let ax = ctx.mul(&x, &self.a);
-        let y_2 = ctx.add(&self.b, &ctx.add(&x_cubic, &ax));
-
-        let mut y = self.fctx.sqrt(&y_2)?;
-
-        // if neg {
-        //     y = self.fctx.neg(&y);
-        // }
-
-        if y.get_value(7) & 0x01 != neg as u32 {
-            y = self.fctx.neg(&y);
-        }
-
-        self.new_point(&x, &y)
-    }
-
-    pub fn cc(&self, s: &BigUint, r: &BigUint, p: &Point) -> Result<Point, Sm2Error> {
-        // let ctx = &self.fctx;
-
-        // let g = self.generator();
-        // let b1 = self.mul(&(s % self.get_n()), &g);
-        // println!("{}", b1);
-
-        let b =  self.g_mul(s);
-        let b = self.neg(&b);
-        let c = self.add(p, &b);
-
-        let x = s + r;
-        let x = self.inv_n(&x);
-        // let a = FieldElem::from_biguint(&x);
-
-        // let a = ctx.add(&FieldElem::from_biguint(s), &FieldElem::from_biguint(r));
-        // let a = ctx.inv(&a);
-
-        let r = self.mul(&x, &c);
-
-        Ok(r)
-    }
-
     pub fn bytes_to_point(&self, b: &[u8]) -> Result<Point, Sm2Error> {
         let ctx = &self.fctx;
 
@@ -650,6 +605,36 @@ impl EccCtx {
         } else {
             Err(Sm2Error::InvalidPublic)
         }
+    }
+
+    pub fn get_point_x(&self, x: &BigUint, neg: u8,) -> Result<Point, Sm2Error> {
+        let ctx = &self.fctx;
+
+        let x = FieldElem::from_biguint(x);
+
+        let x_cubic = ctx.mul(&x, &ctx.mul(&x, &x));
+        let ax = ctx.mul(&x, &self.a);
+        let y_2 = ctx.add(&self.b, &ctx.add(&x_cubic, &ax));
+
+        let mut y = self.fctx.sqrt(&y_2)?;
+
+        if y.get_value(7) & 0x01 != neg as u32 {
+            y = self.fctx.neg(&y);
+        }
+
+        self.new_point(&x, &y)
+    }
+
+    pub fn calc_pubkey(&self, s: &BigUint, r: &BigUint, p: &Point) -> Result<Point, Sm2Error> {
+        // r = (p - sG) / (s + r)
+        let sg =  self.g_mul(s);
+        let sg = self.neg(&sg);
+        let num1 = self.add(p, &sg);
+
+        let denominator = s + r;
+        let num2 = self.inv_n(&denominator);
+
+        Ok(self.mul(&num2, &num1))
     }
 }
 
--- src/sm2/signature.rs
+++ src/sm2/signature.rs
@@ -15,7 +15,6 @@
 use super::ecc::*;
 use super::field::FieldElem;
 use num_bigint::BigUint;
-use num_integer::Integer;
 use num_traits::*;
 use sm3::hash::Sm3Hash;
 
@@ -24,7 +23,7 @@ use yasna;
 use byteorder::{BigEndian, WriteBytesExt};
 use sm2::error::Sm2Error;
 use std::fmt;
-
+use num_integer::Integer;
 pub type Pubkey = Point;
 pub type Seckey = BigUint;
 
@@ -303,89 +302,69 @@ impl SigCtx {
         }
     }
 
-    pub fn sign_raw_rec(&self, digest: &[u8], sk: &BigUint, k: BigUint, ) -> Result<(Signature, u8), Sm2Error> {
+    pub fn sign_raw_recoverable(&self, digest: &[u8], sk: &BigUint, k: BigUint) -> Result<(Signature, u8), Sm2Error> {
         let curve = &self.curve;
         // Get the value "e", which is the hash of message and ID, EC parameters and public key
 
         let e = BigUint::from_bytes_be(digest);
 
-        // two while loops
-        // loop {
-            // k = rand()
-            // (x_1, y_1) = g^kg
-            // let k = self.curve.random_uint();
+        let p_1 = curve.g_mul(&k);
+        let (x_1, y_1) = curve.to_affine(&p_1);
+        
+        let x_1 = x_1.to_biguint();
 
-            let p_1 = curve.g_mul(&k);
-            let (x_1, y_1) = curve.to_affine(&p_1);
-            
-            let x_1 = x_1.to_biguint();
-            // let y_1 = y_1.to_biguint();
+        // r = e + x_1
+        let r_total = &e + x_1;
+        let r = r_total.clone() % curve.get_n();
+        if r == BigUint::zero() || &r + &k == *curve.get_n() {
+            return Err(Sm2Error::InvalidMessage);
+        }
 
-            // r = e + x_1
-            let r_1 = &e + x_1.clone();
-            let r = r_1.clone() % curve.get_n();
-            if r == BigUint::zero() || &r + &k == *curve.get_n() {
-                return Err(Sm2Error::InvalidMessage);
-            }
+        let overflow = r_total.clone() / curve.get_n();
+        let recid = (if overflow.is_zero() { 0 } else { 1 << overflow.to_u8().unwrap() }) | (if y_1.is_even() { 0 } else { 1 });
 
-            let a = r_1.clone() / curve.get_n();
-            let recid = (if r_1 >= *curve.get_n() { 1 << a.to_u8().unwrap() } else {0}) | (if y_1.is_even() { 0 } else {1});
+        // s = (1 + sk)^-1 * (k - r * sk)
+        let s1 = curve.inv_n(&(sk + BigUint::one()));
 
-            // let xxxx = y_1.to_biguint();
-            // println!("yyy: {}", xxxx);
+        let mut s2_1 = &r * sk;
+        if s2_1 < k {
+            s2_1 += curve.get_n();
+        }
+        let mut s2 = s2_1 - k;
+        s2 %= curve.get_n();
+        let s2 = curve.get_n() - s2;
 
-            // s = (1 + sk)^-1 * (k - r * sk)
-            let s1 = curve.inv_n(&(sk + BigUint::one()));
+        let s = (s1 * s2) % curve.get_n();
 
-            let mut s2_1 = &r * sk;
-            if s2_1 < k {
-                s2_1 += curve.get_n();
-            }
-            let mut s2 = s2_1 - k;
-            s2 %= curve.get_n();
-            let s2 = curve.get_n() - s2;
-
-            let s = (s1 * s2) % curve.get_n();
-
-            // if s.clone() * BigUint::from_u32(2).unwrap() > *curve.get_n() {
-            //     println!("yes")
-            // }
-
-            if s != BigUint::zero() {
-                // Output the signature (r, s)
-                return Ok((Signature { r, s }, recid));
-            }
-            
-            Err(Sm2Error::InvalidMessage)
-        // }
+        if s != BigUint::zero() {
+            // Output the signature (r, s)
+            return Ok((Signature { r, s }, recid));
+        }
+        
+        Err(Sm2Error::InvalidMessage)
     }
 
     pub fn recover_raw(&self, msg: &[u8], sig: &Signature, rec_id: u8,) -> Result<Point, Sm2Error> {
-        debug_assert!(rec_id < 4);
-
         if sig.get_r().is_zero() || sig.get_s().is_zero() {
             return Err(Sm2Error::InvalidSignature);
         }
 
         let curve = &self.curve;
 
-        let e = BigUint::from_bytes_be(msg);
-
         let mut x = sig.get_r().clone();
         if rec_id & 4 != 0 {
             x = x + curve.get_n() + curve.get_n();
         } else if rec_id & 2 != 0 {
-            x = x +  curve.get_n();
+            x = x + curve.get_n();
         }
 
+        let e = BigUint::from_bytes_be(msg);
         x = x - e;
 
-        let p = curve.get_point(&x, rec_id & 1)?;
+        let p = curve.get_point_x(&x, rec_id & 1)?;
 
-        let r = curve.cc(sig.get_s(), sig.get_r(), &p)?;
-        
-
-        Ok(r)
+        // r = (p - sG) / (s + r)
+        curve.calc_pubkey(sig.get_s(), sig.get_r(), &p)
     }
 
     pub fn verify(&self, msg: &[u8], pk: &Point, sig: &Signature) -> bool {
